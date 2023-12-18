1306c205fe10f35cd617e68bebc4fa75703b679b
smart-life-tech <christlightworld@gmail.com>
2023-12-18 13:34:07 +0100

    new file:   stt.py

---
+++ stt.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+
+def speech_to_text():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Say something...")
+        audio_data = recognizer.listen(source)
+
+    try:
+        text_result = recognizer.recognize_google(audio_data)
+        print("Speech-to-Text Result:", text_result)
+    except sr.UnknownValueError:
+        print("Speech Recognition could not understand audio.")
+    except sr.RequestError as e:
+        print(f"Could not request results from Google Speech Recognition service; {e}")
+
+if __name__ == "__main__":
+    speech_to_text()
