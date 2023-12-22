b50ddfd4ad6b0a955bd639a8f1f23da375e00402
smart-life-tech <christlightworld@gmail.com>
2023-12-22 09:21:58 +0100

    modified:   stt.py

--- stt.py
+++ stt.py
@@ -3,7 +3,7 @@ import speech_recognition as sr
 def speech_to_text():
     recognizer = sr.Recognizer()
 
-    with sr.Microphone() as source:
+    with sr.Microphone() as source:#here the usb mic of the pi was automatically detected
         print("Say something...")
         audio_data = recognizer.listen(source)
 
